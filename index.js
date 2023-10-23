efc8c2316dab9309024c86c26ab33920ffeb4bec
Miguel Weeks <miguelweeks4@gmail.com>
2023-10-23 16:47:04 +0000

    Added dialog box to create new todos

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const add = document.querySelector(".add")
-const newToDo = document.createElement("div")
-newToDo.classList.add('todo')
+const dialog = document.querySelector('dialog')
+const save = document.querySelector('.save')
+const note = document.querySelector('.container')
 
+add.addEventListener("click", () =>{
+    dialog.style.display = 'flex'
+})
 
-add.addEventListener("click", () => {
-    document.body.appendChild(newToDo)
-    
+save.addEventListener('click', () => {
+    console.log("Saved")
+    dialog.style.display = 'none'
+
+    note.innerHTML = `<div class="todo">
+    <div class="header">
+        <h4>What to do</h4>
+    </div>
+    <div class="thingToDo">
+        <p>This is what I want to achieve </p>
+    </div>
+    <div class="complete">
+        <button class="done">Done</button>
+    </div>`
+    document.body.appendChild(note)
 })
