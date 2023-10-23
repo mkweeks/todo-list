431793c76f78f86cb252ca909f61aca84a6478d4
Miguel Weeks <miguelweeks4@gmail.com>
2023-10-23 17:03:25 +0000

    Get value from text field

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 const add = document.querySelector(".add")
 const dialog = document.querySelector('dialog')
 const save = document.querySelector('.save')
+const todo = document.createElement('div')
 const note = document.querySelector('.container')
 
 add.addEventListener("click", () =>{
-    dialog.style.display = 'flex'
+    dialog.style.display = 'flex';
+
 })
 
 save.addEventListener('click', () => {
     console.log("Saved")
     dialog.style.display = 'none'
 
-    note.innerHTML = `<div class="todo">
-    <div class="header">
-        <h4>What to do</h4>
-    </div>
-    <div class="thingToDo">
-        <p>This is what I want to achieve </p>
-    </div>
-    <div class="complete">
-        <button class="done">Done</button>
-    </div>`
-    document.body.appendChild(note)
+    const value = document.querySelector('input').value
+
 })
